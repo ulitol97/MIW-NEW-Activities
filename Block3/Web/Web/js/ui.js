7aa29d7d557646ab2e0b4ef2e539dde422319f0b
ulitol97 <eduulitol@gmail.com>
2021-01-15 18:38:53 +0100

    Reformat mobile nav.

--- Block3/Web/Web/js/ui.js
+++ Block3/Web/Web/js/ui.js
@@ -1,14 +1,23 @@
+const navElement = document.querySelector("nav");
+const navContentsElement = document.querySelector(".nav-container");
 const navToggleElement = document.getElementById("nav-toggle");
 const navOpenClass = "open";
+const navStickyClass = "sticky";
 const navClosedClass = "closed";
 
 const yearElement = document.getElementById("year");
 
 // Toggle nav on click
 function toggleNav() {
-  if (navToggleElement.classList.contains(navOpenClass))
+  if (navToggleElement.classList.contains(navOpenClass)) {
+    // navContentsElement.classList.add("hidden");
+    navElement.classList.add(navStickyClass);
     navToggleElement.classList.replace(navOpenClass, navClosedClass);
-  else navToggleElement.classList.replace(navClosedClass, navOpenClass);
+  } else {
+    // navContentsElement.classList.remove("hidden");
+    navElement.classList.remove(navStickyClass);
+    navToggleElement.classList.replace(navClosedClass, navOpenClass);
+  }
 }
 
 navToggleElement.addEventListener("click", toggleNav);
